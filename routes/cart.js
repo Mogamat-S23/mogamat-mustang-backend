2f88c743d481dfa12cce385da2523d8c95710c4e
Mogamat-S23 <saleemariefdien@gmail.com>
2022-08-24 15:06:05 +0200

    add to cart

--- routes/cart.js
+++ routes/cart.js
@@ -22,8 +22,39 @@ router.get("/users/:id/cart", bodyparser.json(),(req,res)=>{
     })
 })
 
+////////
 
-
+// add to cart
+router.post('/users/:id/cart', bodyparser.json(), (req, res) => {
+    let cart = `select cart from user where user_id = ${req.params.id};`
+    db.query(cart, (err, results) => {
+        if (err) throw err
+        if (results.length > 0) {
+            let cart
+        }
+        if (results[0].length === null) {
+            cart = []
+        } else {
+            cart = JSON.parse(results[0].cart)
+        }
+        let {
+            product_id
+        } = req.body
+        let product = `Select * from products where product_id = ?`;
+        db.query(product, product_id, (err, productData) => {
+            if (err) throw err
+            cart.push(productData)
+            console.log(cart);
+            let updateCart = `UPDATE user SET cart = ? WHERE user_id = ${req.params.id}`
+            db.query(updateCart, JSON.stringify(cart), (err, results) => {
+                if (err) throw err
+                res.json({
+                    cart: results
+                })
+            })
+        })
+    })
+})
 
 module.exports = router
 
